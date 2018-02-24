8621ae5d1c41000cc37dff47f3a38d9f81d65256
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2018-02-24 09:15:27 +0100

    update sass file (gif-tag.scss) for compilation by jeykyll, and add some documentations

--- push-gh-pages.js
+++ push-gh-pages.js
@@ -8,3 +8,43 @@ ghpages.publish('_site', function(err) {
   }
 });
 
+/*
+
+# Gemfile
+
+source "https://rubygems.org"
+
+# gem 'github-pages', group: :jekyll_plugins
+#   limitation plugins : gif_tag not in list https://pages.github.com/versions/ (writing on server...)
+#   and default running in safe mode (for github pages)
+#   see : https://github.com/jekyll/jekyll/issues/5265
+#   see : https://help.github.com/enterprise/2.12/user/articles/adding-jekyll-plugins-to-a-github-pages-site/
+#   "Other plugins are not supported, so the only way to incorporate them in your site is to generate your site locally
+#    and then push your site's static files to your GitHub Pages site."
+
+gem 'jekyll', group: :jekyll_plugins
+
+# end Gemfile
+
+# after, here my process for pushing _site directory on github :
+
+# 1/ (install npm package)
+#  npm install gh-pages --save-dev
+
+# 2/ (create js file)
+# this file
+
+# 3/ (change pages publishing source from master branch to gh-pages branch)
+# see :  https://help.github.com/articles/configuring-a-publishing-source-for-github-page
+
+# 4/ (build _site)
+# bundle exec jekyll build
+
+# 5/ (start script to push - idem, from root project/site)
+#  ... waiting a little the first time
+# $ nodejs push-gh-pages.js
+
+site is published !
+https://ocapuozzo.github.io/p4guitar.org/
+
+*/
